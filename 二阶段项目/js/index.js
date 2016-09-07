e803b53a885cd4a2c93c8a5837fe54b638865525
yanchengye <984639084@qq.com>
2016-09-07 08:39:15 +0800

    9.7

--- 二阶段项目/js/index.js
+++ 二阶段项目/js/index.js
@@ -9,9 +9,11 @@ $(function(){
 		$(this).css('background','url(img/banner'+(i+1)+'.jpg) no-repeat center top');
 	});
 	var $index,$nextindex;                      //定义变量保存下标
-	setInterval(function(){
+	timer=setInterval(function(){
 		$index=$('#banner>.active').index();                //获取下标
 		$nextindex=$index+1;
+		$('.banner-squlist').removeClass('active');
+		$('.banner-squlist').eq($nextindex).addClass('active');
 		if($index>$('#banner>li').size()-1){$index=0;};       //边界判断
 		if($nextindex>$('#banner>li').size()-1){$nextindex=0;};
 		$('#banner>li').eq($index).fadeOut(500);
@@ -19,6 +21,30 @@ $(function(){
 		$('#banner>li').removeClass('active');
 		$('#banner>li').eq($nextindex).addClass('active');
 	},4000)
+	$('.banner-square').on('mouseenter',function(){
+		clearInterval(timer);
+	})
+	$('.banner-square').on('mouseleave',function(){
+		timer=setInterval(function(){
+			$index=$('#banner>.active').index();                //获取下标
+			$nextindex=$index+1;
+			$('.banner-squlist').removeClass('active');
+			$('.banner-squlist').eq($nextindex).addClass('active');
+			if($index>$('#banner>li').size()-1){$index=0;};       //边界判断
+			if($nextindex>$('#banner>li').size()-1){$nextindex=0;};
+			$('#banner>li').eq($index).fadeOut(500);
+			$('#banner>li').eq($nextindex).fadeIn(500);
+			$('#banner>li').removeClass('active');
+			$('#banner>li').eq($nextindex).addClass('active');
+		},4000)
+	}) 
+	$('.banner-squlist').on('click',function(){               //点击方块查看对应banner
+		$('.banner-squlist').removeClass('active'); 
+		$(this).addClass('active');
+		$index=$(this).index();
+		$('#banner>li').fadeOut(500);
+		$('#banner>li').eq($index).fadeIn(500);
+	})
 })
 
 //菜单
@@ -68,28 +94,38 @@ $(function(){
 
 
 //楼层下面的品牌点击
+function floorad(objul,obja,objli,objbtn,objbtnlist){
+	objul.css('width',(objli.outerWidth())*(objul.children().length))
+	obja.eq(1).on('click',function(){    //点击右边的向左拉
+		if(objul.position().left<=-((objli.outerWidth())*(objul.children().length-10))){
+			objul.css('left',objul.position().left);
+		}else{
+			objul.animate({'left':objul.position().left-122},300);		
+		}
+	})
+	obja.eq(0).on('click',function(){        //点击左键向右拉
+		if(objul.position().left>=0){
+			objul.css('left',0);
+		}else{
+			objul.animate({'left':objul.position().left+122},300);
+		}
+	})
+	objbtn.on('mouseenter',function(){        //鼠标移入显示按钮
+		objbtnlist.css('display','block');
+	})
+	objbtn.on('mouseleave',function(){           //鼠标移出隐藏按钮
+		objbtnlist.css('display','none');
+	})
+}
 $(function(){
-	$('.index-1f-bottom ul').css('width',($('.index-1f-bottom ul li').outerWidth())*($('.index-1f-bottom ul').children().length))
-	$('.index-1f-bottombtn a').eq(1).on('click',function(){    //点击右边的向左拉
-		if($('.index-1f-bottom ul').position().left<=-(($('#index-1f .index-1f-bottom ul li').outerWidth())*($('.index-1f-bottom ul').children().length-10))){
-			$('.index-1f-bottom ul').css('left',$('.index-1f-bottom ul').position().left);
-		}else{
-			$('.index-1f-bottom ul').animate({'left':$('.index-1f-bottom ul').position().left-122},300);		
-		}
-	})
-	$('.index-1f-bottombtn a').eq(0).on('click',function(){        //点击左键向右拉
-		if($('.index-1f-bottom ul').position().left>=0){
-			$('.index-1f-bottom ul').css('left',0);
-		}else{
-			$('.index-1f-bottom ul').animate({'left':$('.index-1f-bottom ul').position().left+122},300);
-		}
-	})
-	$('.index-1f-bottom').on('mouseenter',function(){        //鼠标移入显示按钮
-		$('.index-1f-bottombtn').css('display','block');
-	})
-	$('.index-1f-bottom').on('mouseleave',function(){           //鼠标移出隐藏按钮
-		$('.index-1f-bottombtn').css('display','none');
-	})
+	floorad($('#index-1f .index-1f-bottom ul'),$('#index-1f .index-1f-bottombtn a'),$('#index-1f .index-1f-bottom ul li'),$('#index-1f .index-1f-bottom'),$('#index-1f .index-1f-bottombtn'));
+	floorad($('#index-2f .index-1f-bottom ul'),$('#index-2f .index-1f-bottombtn a'),$('#index-2f .index-1f-bottom ul li'),$('#index-2f .index-1f-bottom'),$('#index-2f .index-1f-bottombtn'));
+	floorad($('#index-3f .index-1f-bottom ul'),$('#index-3f .index-1f-bottombtn a'),$('#index-3f .index-1f-bottom ul li'),$('#index-3f .index-1f-bottom'),$('#index-3f .index-1f-bottombtn'));
+	floorad($('#index-4f .index-1f-bottom ul'),$('#index-4f .index-1f-bottombtn a'),$('#index-4f .index-1f-bottom ul li'),$('#index-4f .index-1f-bottom'),$('#index-4f .index-1f-bottombtn'));
+	floorad($('#index-5f .index-1f-bottom ul'),$('#index-5f .index-1f-bottombtn a'),$('#index-5f .index-1f-bottom ul li'),$('#index-5f .index-1f-bottom'),$('#index-5f .index-1f-bottombtn'));
+	floorad($('#index-6f .index-1f-bottom ul'),$('#index-6f .index-1f-bottombtn a'),$('#index-6f .index-1f-bottom ul li'),$('#index-6f .index-1f-bottom'),$('#index-6f .index-1f-bottombtn'));
+	floorad($('#index-7f .index-1f-bottom ul'),$('#index-7f .index-1f-bottombtn a'),$('#index-7f .index-1f-bottom ul li'),$('#index-7f .index-1f-bottom'),$('#index-7f .index-1f-bottombtn'));
+	floorad($('#index-8f .index-1f-bottom ul'),$('#index-8f .index-1f-bottombtn a'),$('#index-8f .index-1f-bottom ul li'),$('#index-8f .index-1f-bottom'),$('#index-8f .index-1f-bottombtn'));
 })
 
 //楼层切换
@@ -103,6 +139,19 @@ $(function(){
 	})
 	$(window).on('scroll',function(){                           //窗口滚动时事件
 
+	})
+})
+
+//固定右侧栏
+$(function(){
+	$('#index-fixed>div').hover(function(){
+		$(this).children().css('display','block');
+		$(this).find($('.before')).stop(true,true).animate({'top':-38},200);
+		$(this).find($('.after')).stop(true,true).animate({'top':0},200);
+	},function(){
+		$(this).find($('.hide')).css('display','none');
+		$(this).find($('.before')).stop(true,true).animate({'top':0},200);
+		$(this).find($('.after')).stop(true,true).animate({'top':38},200);
 	})
 })
 
@@ -122,4 +171,88 @@ $(function(){
 		$('.banner-blog div').css('display','none');
 		$('.banner-blog div').eq($(this).index()).css('display','block');
 	})
+})
+
+//白酒的点击事件
+$(function(){
+	$('.wine-spirit-btn .right').on('click',function(){  //白酒切换
+		$('.wine-spirit1').animate({'left':-1200});
+		$('.wine-spirit2').animate({'right':0});
+	})
+	$('.wine-spirit-btn .left').on('click',function(){
+		$('.wine-spirit1').animate({'left':15});
+		$('.wine-spirit2').animate({'right':-1183});
+	})
+	/*$('.wine-spirit-wrap').hover(function(){              //鼠标移入 显示按钮
+		$('.wine-spirit-btn').css('display','block');
+	},function(){
+		$('.wine-spirit-btn').css('display','none');
+	})*/
+	$('#wine > a').eq(0).on('mouseenter',function(){
+		$('#wine > a').removeClass('wine-active');
+		$(this).addClass('wine-active');
+		$('.wine-spirit').css('display','none');
+		$('.wine-spirit-wrap').css('display','block');
+		$('.wine-spirit1').css('display','block');
+		$('.wine-spirit2').css('display','block');
+	})
+	$('#wine > a').eq(1).on('mouseenter',function(){
+		$('#wine > a').removeClass('wine-active');
+		$(this).addClass('wine-active');
+		$('.wine-spirit').css('display','none');
+		$('.wine-spirit-wrap').css('display','none');
+		$('.wine-local').css('display','block');
+	})
+	$('#wine > a').eq(2).on('mouseenter',function(){
+		$('#wine > a').removeClass('wine-active');
+		$(this).addClass('wine-active');
+		$('.wine-spirit-wrap').css('display','none');
+		$('.wine-red').css('display','block');
+	})
+})
+
+$(function(){                                       //热门店铺
+	$('.hotstores-center a').hover(function(){
+		$(this).stop(true,true).animate({'top':-10},300).animate({'top':0},300);
+	},function(){
+
+	})
+})
+
+$(function(){
+	$('.tablist').hover(function(){                   //疯狂抢购
+		$('.tablist').removeClass('active');
+		$(this).addClass('active');
+		$index=$(this).index();
+		$('.tabitem').removeClass('act');
+		$('.tabitem').eq($index).addClass('act');	
+	},function(){
+
+	})
+})
+function floorchoose(obj,obj2){
+	obj.hover(function(){
+		obj.removeClass('active');
+		$(this).addClass('active');
+		$index=$(this).index();
+		obj2.removeClass('active');
+		obj2.eq($index).addClass('active');
+	})
+}
+$(function(){                                                            //楼层的选项卡
+	/*$('#index-1f .tab a').hover(function(){
+		$('#index-1f .tab a').removeClass('active');
+		$(this).addClass('active');
+		$index=$(this).index();
+		$('#index-1f .index-1f-middle-goods').removeClass('active');
+		$('#index-1f .index-1f-middle-goods').eq($index).addClass('active');
+	})*/
+	floorchoose($('#index-1f .tab a'),$('#index-1f .index-1f-middle-goods')); //一楼的
+	floorchoose($('#index-2f .tab a'),$('#index-2f .index-1f-middle-goods'));
+	floorchoose($('#index-3f .tab a'),$('#index-3f .index-1f-middle-goods'));
+	floorchoose($('#index-4f .tab a'),$('#index-4f .index-1f-middle-goods'));
+	floorchoose($('#index-5f .tab a'),$('#index-5f .index-1f-middle-goods'));
+	floorchoose($('#index-6f .tab a'),$('#index-6f .index-1f-middle-goods'));
+	floorchoose($('#index-7f .tab a'),$('#index-7f .index-1f-middle-goods'));
+	floorchoose($('#index-8f .tab a'),$('#index-8f .index-1f-middle-goods'));
 })
